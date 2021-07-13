84c6b87c1ae6ef77c2fd902b4c49d36e67f1a9de
SUNIL KUMAR <50536429+TharVid@users.noreply.github.com>
2021-07-13 18:22:52 +0530

    update

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Freelancer", "Developer", "Blogger", "Student", "Marketer"],
+        strings: ["Freelancer", "Security Engineer", "MCT", "Marketer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Freelancer", "Developer", "Blogger", "Student", "Marketer"],
+        strings: ["Freelancer", "Security Engineer", "MCT", "Marketer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
